7cee88424aa22e95e1a3f658e4d1dc2bb849962e
Jose Pablo Flores <jflores50158@ufide.ac.cr>
2024-07-08 15:23:19 -0600

    FINAL FINAL

--- TCU/src/main/java/com/TCU/ProjectConfig.java
+++ TCU/src/main/java/com/TCU/ProjectConfig.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.TCU;
 
 import com.TCU.service.AyudaService;
@@ -11,26 +8,23 @@ import jakarta.annotation.PostConstruct;
 import java.time.LocalDate;
 import java.util.Locale;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ResourceBundleMessageSource;
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
-/**
- *
- * @author jp09f
- */
+
 @Configuration
 public class ProjectConfig implements WebMvcConfigurer {
 
